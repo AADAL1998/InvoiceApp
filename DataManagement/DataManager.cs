4b3e75de53fad028c490b5d89a071ed664f173c0
aadal <aadalarasugnanasekar@gmail.com>
2024-06-25 10:19:46 +0530

    Add project files.

---
+++ DataManagement/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.DataManagement
+{
+    public static class DataManager
+    {
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static List<Category> Categories { get; set; } = new List<Category>();
+        public static List<Customer> Customers { get; set; } = new List<Customer>();
+        public static List<Invoice> Invoices { get; set; } = new List<Invoice>();
+
+    }
+}
